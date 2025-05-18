3427b5a4448520cd9ead03e929ed477a9d63880f
smishr31 <sajalmishra910@gmail.com>
2025-05-18 19:45:05 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,11 @@
+a= int(input("Enter first number = "))
+b= int(input("Enter second number = "))
+
+Add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print("Addition = ", Add)
+print("substraction = ",sub)
+print("multiplication = ",mul)
+print("division = ",div)
---
+++ Task_2.py
@@ -0,0 +1,4 @@
+a = input("Enter your First Name: ")
+b = input("Enter your Second Name: ")
+c= a+b
+print(f"Helo {c}! How you are doing? ")
